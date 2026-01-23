37471e625629290ccb595709e43a3c072a13be16
Anton Olshevskij <aolshevskij@conteq.com>
2026-01-23 09:04:38 +0300

    add Parallel Routes and group routes

---
+++ app/(root)/@modal/(.)product/[id]/page.tsx
@@ -0,0 +1,25 @@
+import { ChooseProductModal } from '@/components/shared'
+import { prisma } from '@/prisma/prisma'
+import { notFound } from 'next/navigation'
+
+export default async function ProductModalPage({
+	params: { id },
+}: {
+	params: { id: string }
+}) {
+	const product = await prisma.product.findFirst({
+		where: {
+			id: Number(id),
+		},
+		include: {
+			ingredients: true,
+			items: true,
+		},
+	})
+
+	if (!product) return notFound()
+
+	return (
+		<ChooseProductModal product={product} className="flex flex-col my-10" />
+	)
+}
